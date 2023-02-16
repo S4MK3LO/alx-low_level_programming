9412bdc671bebc349ce7aabf98a0fff6a7c85bbb
S4MK3LO <samkelomncwango19@gmail.com>
2023-02-16 08:29:59 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," followed newline 
+ * main - prints "with proper grammar, but the outcome is 
+ * a piece of art," followed newline 
  * Return: Always 0.
  *
  */
