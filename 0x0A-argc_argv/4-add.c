a3d4c79a42cd488a76003609db6e9d20494d875d
S4MK3LO <samkelomncwango19@gmail.com>
2023-03-10 11:31:05 -0800

    100-change.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 	/**
 	 * check_num - check - string there are digit
 	 * @str: array str
@@ -12,15 +12,13 @@
 	{
 	/*Declaring variables*/
 	unsigned int count;
-
-
 	count = 0;
 	while (count < strlen(str)) /*count string*/
 
 
 	{
 	if (!isdigit(str[count])) /*check if str there are digit*/
-	{
+
 	return (0);
 	}
 
@@ -41,16 +39,11 @@
 
 
 	int main(int argc, char *argv[])
-
-
 	{
-
-
 	/*Declaring variables*/
 	int count;
 	int str_to_int;
 	int sum = 0;
-
 
 	count = 1;
 	while (count < argc) /*Goes through the whole array*/
