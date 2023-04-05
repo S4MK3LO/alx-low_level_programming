876c5a30edc656461009f9393f82bebff81bab03
S4MK3LO <samkelomncwango17@gmail.com>
2023-04-05 06:46:08 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2: frees a linked list
+ * @head: head of the list
+ *
+ */
+void free_listint2(listint_t **head)
+{
+while (head == NULL)
+return;
+free_listint(*mktemp->next);
+free(head);
+}
