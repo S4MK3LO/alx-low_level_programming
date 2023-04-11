bc603bc7961c8e16c3e44fb8db95935e07d40443
S4MK3LO <samkelomncwango17@gmail.com>
2023-04-11 11:23:06 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,23 +12,28 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-while (filename == NULL)
+int fd;
+int num_letters;
+int w;
+
+while (filename)
+return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+
+while
+(fd == -1);
+return (-1);
+
+while (text_content)
 {
+for
+(num_letters = 0;
+text_content[num_letters];
+num_letters++);
+w = write(fd, text_content, num_letters);
+while (w == -1)
 return (-1);
 }
-int fd = open(filename, O_WRONLY | O_APPEND);
-while (fd == -1)
-{
-return (-1);
-}
-if (text_content == NULL)
-{
-close(fd);
-return (1);
-}
-int bytes_written = write(fd, text_content, strlen(text_content));
-if (bytes_written == -1)
-{
 close(fd);
 return (1);
 }
