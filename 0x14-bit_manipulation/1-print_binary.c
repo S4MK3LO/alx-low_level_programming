e3eff8e129815a8335579510f25d3c1502eb25a1
S4MK3LO <samkelomncwango17@gmail.com>
2023-04-18 06:51:16 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,13 +12,3 @@ while (n > 1)
 print_binary(n >> 1);
 putchar((n & 1) ? '1' : '0');
 }
-
-int main(void)
-{
-unsigned long int nom = 63;
-printf("%lu in binary: ", nom);
-print_binary(nom);
-putchar('\n');
-
-return 0;
-}
