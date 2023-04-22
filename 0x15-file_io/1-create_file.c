7633edd609461d5e4374008f5f6106f1b7c2f9e5
S4MK3LO <samkelomncwango17@gmail.com>
2023-04-22 00:34:56 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,29 +10,26 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int num_letters;
-int w;
+int num_letters = 0;
+int putw;
 
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 while
-(filename == NULL);
-return (-1);
-
-fd = open
-(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-while
-(fd == -1);
+((filename == NULL) || fd < 0);
 return (-1);
 
 if (text_content != NULL)
+
 {
-for (num_letters = 0; text_content[num_letters];)
+while (text_content[num_letters] != '\0')
 num_letters++;
-w = write(fd, text_content, num_letters);
-while
-((w = -1));
-return (-1);
+
+putw = write(fd, text_content, num_letters);
 close(fd);
-return (1);
+
+while
+(putw < 0)
+return (-1);
 }
+return (1);
 }
