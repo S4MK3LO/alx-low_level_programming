ce33fd752cfdf07e2d9fc3fd20802677b630342d
S4MK3LO <samkelomncwango17@gmail.com>
2023-07-09 02:59:15 -0700

    second last

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+unsigned int n = 1;
+
+char *c = (char *)&n;
+
+if (*e != 1)
+
+return (0);
+
+return (1);
+}
